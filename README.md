768d9ca36c3ee76289fbc0044b7f3a188577da40
arinaaunaka <arinaaunaka@gmail.com>
2022-11-15 18:23:33 +0700

    Tugas 8: Flutter Form

--- README.md
+++ README.md
@@ -31,3 +31,47 @@ Keduanya dapat digunakan untuk membuat variabel yang bersifat immutable. Namun, 
 3. Menambahkan fungsi `_decrementCounter` yang mengurangi angka sebanyak satu satuan dan di-set sebagai `onPressed` pada tombol -, sedangkan fungsi `_incrementCounter` yang menambahkan angka sebanyak satu satuan di-set sebagai `onPressed` pada tombol +.
 4. Dengan menggunakan if-else, jika counter bernilai genap maka Text yang akan ditampilkan adalah GENAP dengan `style` berwarna merah, dan jika counter bernilai ganjil maka Text yang akan ditampilkan adalah GANJIL dengan `style` berwarna biru.
 5. Bonus: dengan menggunakan if, jika counter bernilai 0, maka tombol yang ditampilkan hanya tombol + saja.
+
+
+# **Tugas 8: Flutter Form**
+
+## Perbedaan `Navigator.push` dan `Navigator.pushReplacement`
+`Navigator.push` akan menambahkan rute/page baru diatas page sebelumnya (tanpa menghapus yang sebelumnya), sehingga seperti tumpukan layer page. Sedangkan, `Navigator.pushReplacement` akan me-*replace* page saat ini dengan page yang baru. 
+
+## Widget dan Fungsinya
+- `Drawer`: widget untuk membuat drawer Material Design.
+- `ListTile`: widget untuk membuat list tile.
+- `Navigator`: widget yang mengelola sekumpulan child widget dengan prinsip stack.
+- `TextEditingController`: widget untuk membuat controller untuk text field yang dapat diedit.
+- `Form`: widget yang membuat container untuk form field.
+- `Container`: widget yang berfungsi sebagai container untuk menampung widget lainnya.
+- `TextFormField`: widget untuk membuat form field yang berisi Text Field.
+- `SizedBox`: widget untuk membuat fixed size box.
+- `DropdownButtonFormField`: widget untuk membuat widget DropdownButton yang dibungkus dengan InputDecorator.
+- `TextButton`: widget untuk membuat text button.
+- `Align`: widget untuk membuat penyelarasan widget.
+- `ListView`: widget untuk membuat array widget linier yang scrollable dari sebuah list.
+- `Card`: widget untuk membuat card Material Design.
+
+## Jenis-jenis Event pada Flutter
+- `onPressed`
+- `onChanged`
+- `onSaved`
+- `onTap`
+- `onPressed`
+- `onLongPress`
+- `onSubmitted`
+- `onEditingComplete`
+- `onLoading`
+- `onRefresh`
+- `onPointerMove`
+
+## Cara Kerja `Navigator`
+`Navigator` bekerja seperti stack, yaitu menggunakan prinsip LIFO (Last-In, First-Out) dimana layar yang ditampilkan saat ini adalah page yang berada di top of stack dari stack page (tumpukan layar) tersebut. 
+
+## Implementasi Checklist
+1. Bonus: membuat drawer.dart yang berisikan Drawer dengan 3 tombol navigasi, yaitu halaman counter (`main.dart`), halaman form (`form.dart`), halaman yang menampilkan data budget (`databudget.dart`).
+2. Menambahkan drawer ke tiap file-file: `main.dart`, `form.dart`, dan `databudget.dart`.
+3. Membuat `budget.dart` untuk model Budget.
+4. Membuat `form.dart` yang berisikan form field untuk diisi user dengan `TextFormField` untuk judul dan nominal budget, `DropdownButtonFormField` untuk jenis budget, `showDatePicker` untuk date budget (bonus), dan `TextButton` untuk menyimpan budget ke dalam list budget dan menampilkan datanya dalam sebuah pop up dialog. 
+5. Membuat `databudget.dart` untuk mengambil data budget dari list budget dan menampilkannya dalam bentuk card.
---
+++ lib/databudget.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:counter_7/drawer.dart';
+import 'package:counter_7/budget.dart';
+
+class MyDataPage extends StatefulWidget {
+  const MyDataPage({super.key});
+
+  @override
+  State<MyDataPage> createState() => _MyDataPageState();
+}
+
+class _MyDataPageState extends State<MyDataPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Data Budget'),
+        ),
+        drawer: const MyDrawer(),
+        body: SingleChildScrollView(
+            child: Container(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                    children: ListBudget.budgetList.isNotEmpty
+                        ? ListBudget.budgetList
+                            .map((budget) => Card(
+                                  shape: const RoundedRectangleBorder(
+                                    side: BorderSide(
+                                      color: Colors.blue,
+                                    ),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(12)),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(15.0),
+                                    child: Column(
+                                      children: [
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(budget.judul,
+                                                style: const TextStyle(
+                                                    fontSize: 16,
+                                                    fontWeight:
+                                                        FontWeight.bold)),
+                                            Text(
+                                                "${budget.date.day}-${budget.date.month}-${budget.date.year}"),
+                                          ],
+                                        ),
+                                        const SizedBox(height: 15),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(budget.nominal.toString()),
+                                            Text(
+                                              budget.jenis,
+                                              style: TextStyle(
+                                                  color: budget.jenis ==
+                                                          "Pemasukan"
+                                                      ? Colors.green
+                                                      : Colors.red),
+                                            ),
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ))
+                            .toList()
+                        : [
+                            Center(
+                                child: Column(children: const [
+                              Text(
+                                "Belum ada data budget yang ditambahkan",
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ]))
+                          ]))));
+  }
+}
